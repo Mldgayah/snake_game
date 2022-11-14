2902c9e4f2bc910bf71f9f1faf5821536bbc8ecf
= <=>
2022-11-14 18:30:20 +0100

    Dplacement du serpent avec les touches

--- scripts.js
+++ scripts.js
@@ -17,15 +17,16 @@ window.onload = function()
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
-        snakee = new Snake([[6,4],[5,4],[4,4]]);
+        snakee = new Snake([[6,4],[5,4],[4,4]], "right");
         refreshCanvas();
     }
 
     function refreshCanvas()
     {
         ctx.clearRect(0,0,canvasWidth, canvasHeight);
-        snakee.draw();
+        ctx.fillStyle = "#ff0000";
         snakee.advance();
+        snakee.draw();
         setTimeout(refreshCanvas, delay);
     }
 
@@ -35,12 +36,11 @@ window.onload = function()
         var y = position[1] * blockSize;
         ctx.fillRect(x, y, blockSize, blockSize);
     }
-
-
-    function Snake(body)
+    function Snake(body, direction)
     {
         this.body = body;
-        this.draw = function()
+        this.direction = direction; 
+        this.draw = function()     //On applique une methode pour dessiner Le serpent en creant une fonction drawBlock
         {
             ctx.save();
             ctx.fillStyle = "#ff0000";
@@ -54,10 +54,76 @@ window.onload = function()
         this.advance = function()
         {
             var nextPosition = this.body[0].slice();
-            nextPosition += 1;
-            this.body.unshift(nextPosition);
-            this.body.pop();
-        }
+            switch(this.direction)
+            {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                default:
+                    throw("Invalid direction");
+
+            }
+            this.body.unshift(nextPosition);//unshift ajoute de nouveaux elements à un tableau
+            this.body.pop(); // Pop supprime le dernier élement d'un tableau
+        };
+        this.setDirection = function(newDirection)
+        {
+            var allowedDirections;
+            switch(this.direction)
+            {
+                case "left":
+                case "right":
+                    allowedDirections("up","down");
+                    break;
+                case "up":
+                case "down":
+                    allowedDirections("left","right");
+                    break;
+                default:
+                    throw("Invalid direction");
+
+            }
+            if (allowedDirections.indexOf(newDirection) > -1)
+            {
+                this.direction = newDirection;
+            }
+        };
     }
+
+    document.onkeydown = function handleKeyDown(e)
+    {
+        var key = e.keyCode;
+        var newDirection;
+        switch(key)
+        {
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            default:
+                return;
+        }
+        snakee.setDirection(newDirection);
+
+
+    }
+
 
 }
